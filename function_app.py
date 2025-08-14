8ebb612938aa64a806be7450d54bcdcd04e1934c
Joe K <jkrilov@gmail.com>
2025-08-14 15:17:13 -0400

    feat: add logging

--- function_app.py
+++ function_app.py
@@ -10,11 +10,15 @@ def strava_webhook(req: func.HttpRequest) -> func.HttpResponse:
     """ Webhook for Strava API subscription """
     logging.info('Python HTTP trigger function processed a request.')
 
+    # log request body
+    logging.info("Request body: %s", req.get_body().decode())
+
     token = req.params.get('hub.verify_token')
     challenge = req.params.get('hub.challenge')
     mode = req.params.get('hub.mode')
 
     if token == "token" and mode == "subscribe" and challenge:
+        logging.info("Received valid request: mode=%s, token=%s, challenge=%s", mode, token, challenge)
         # return a 200 response with the challenge in application/json format
         return func.HttpResponse(
             json.dumps({"hub.challenge": challenge}),
@@ -22,6 +26,7 @@ def strava_webhook(req: func.HttpRequest) -> func.HttpResponse:
             mimetype="application/json"
         )
     else:
+        logging.warning("Received invalid request: mode=%s, token=%s, challenge=%s", mode, token, challenge)
         return func.HttpResponse(
             "Invalid request",
             status_code=400
