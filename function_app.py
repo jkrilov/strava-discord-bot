c6b00b31f5629d27ec26ef22d39172890a66f780
Joe K <jkrilov@gmail.com>
2025-08-14 12:49:59 -0400

    feat: verify token

--- function_app.py
+++ function_app.py
@@ -8,19 +8,22 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 def strava_webhook(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    token = req.params.get('VERIFY_TOKEN')
+    if not token:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            token = req_body.get('VERIFY_TOKEN')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if token == "STRAVA":
+        return func.HttpResponse(
+            "Validated token successfully.",
+            status_code=200
+        )
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+             "Missing token",
+             status_code=400
         )
