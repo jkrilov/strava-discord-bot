a651d1fed6f1a6365b3c807379549c1951da96b5
Joe K <jkrilov@gmail.com>
2025-08-14 15:06:28 -0400

    fix: case sensitivity

--- function_app.py
+++ function_app.py
@@ -8,16 +8,16 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 def strava_webhook(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    token = req.params.get('VERIFY_TOKEN')
+    token = req.params.get('verify_token')
     if not token:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            token = req_body.get('VERIFY_TOKEN')
+            token = req_body.get('verify_token')
 
-    if token == "STRAVA":
+    if token.lower() == "strava":
         return func.HttpResponse(
             "Validated token successfully.",
             status_code=200
